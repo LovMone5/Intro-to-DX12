2be702e5c6fd7eee2ba8845e89445a14f62a2056
lovmone <unimlovmone@gmail.com>
2024-07-11 23:29:35 +0800

    Vector learning
    Learn vector concepts and basic usage in DX

---
+++ 1. Vector/1. Vector/Main.cpp
@@ -0,0 +1,51 @@
+﻿#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <iostream>
+
+using namespace std;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+ostream& XM_CALLCONV operator<<(ostream& out, FXMVECTOR& v) {
+	XMFLOAT3 t;
+	XMStoreFloat3(&t, v);
+	out << "(" << t.x << ", " << t.y << ", " << t.z << ")";
+
+	return out;
+}
+
+int main() {
+	if (!XMVerifyCPUSupport()) {
+		cout << "DirectX math not supported!" << endl;
+		return 0;
+	}
+	auto p = XMVectorZero();
+	auto q = XMVectorSplatOne();
+	auto u = XMVectorSet(1.0f, 2.0f, 3.0f, 0.0f);
+	auto v = XMVectorReplicate(-2.0f);
+	auto w = XMVectorSplatZ(u);
+
+	cout << "p = " << p << endl;
+	cout << "q = " << q << endl;
+	cout << "u = " << u << endl;
+	cout << "v = " << v << endl;
+	cout << "w = " << w << endl;
+
+	cout << "u + v = " << u + v << endl;
+	cout << "10.f * u = " << 10.f * u << endl;
+
+	XMVECTOR proj, perp;
+	auto n = XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f);
+	XMVector3ComponentsFromNormal(&proj, &perp, v, u);
+	cout << "proj = " << proj << endl;
+	cout << "perp = " << perp << endl;
+	
+
+	cout << "||proj|| = " << XMVector3Length(proj) << endl;
+	cout << "Estimate ||proj|| = " << XMVector3LengthEst(proj) << endl;
+	proj = XMVector3Normalize(proj);
+	cout << "proj / ||proj|| = " << proj << endl;
+
+
+	return 0;
+}

e918622ce318e1035e969f5e5f7fcb8f0660a468
lovmone <unimlovmone@gmail.com>
2024-08-21 19:55:01 +0800

    Create a demo of multiple shapes with lighting effects

---
+++ 8. LitShapes/Shaders/color.hlsl
@@ -0,0 +1,89 @@
+#ifndef NUM_DIR_LIGHTS
+    #define NUM_DIR_LIGHTS 3
+#endif
+
+#ifndef NUM_POINT_LIGHTS
+    #define NUM_POINT_LIGHTS 0
+#endif
+
+#ifndef NUM_SPOT_LIGHTS
+    #define NUM_SPOT_LIGHTS 0
+#endif
+
+#include "LightingUtil.hlsl"
+
+cbuffer cbPerObject : register(b0)
+{
+    float4x4 gWorld;
+};
+
+cbuffer cbPass : register(b1)
+{
+    float4x4 gView;
+    float4x4 gInvView;
+    float4x4 gProj;
+    float4x4 gInvProj;
+    float4x4 gViewProj;
+    float4x4 gInvViewProj;
+    float3 gEyePosW;
+    float cbPerObjectPad1;
+    float2 gRenderTargetSize;
+    float2 gInvRenderTargetSize;
+    float gNearZ;
+    float gFarZ;
+    float gTotalTime;
+    float gDeltaTime;
+    float4 gAmbientLight;
+    
+    Light gLights[MaxLights];
+};
+
+cbuffer cbMaterial : register(b2)
+{
+    float4 gDiffuseAlbedo;
+    float3 gFresnelR0;
+    float gRoughness;
+}
+
+struct VertexIn
+{
+    float3 PosL : POSITION;
+    float3 NormalL : NORMAL;
+};
+
+struct VertexOut
+{
+    float4 PosH : SV_POSITION;
+    float3 PosW : POSITIONT;
+    float3 NormalW : NORMAL;
+};
+
+VertexOut VS(VertexIn vin)
+{
+    VertexOut vout;
+    
+    // Transform to homogeneous clip space.
+    float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+    vout.PosW = posW.xyz;
+    vout.NormalW = mul(vin.NormalL, (float3x3)gWorld);
+    
+    vout.PosH = mul(posW, gViewProj);
+    
+    return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{
+    pin.NormalW = normalize(pin.NormalW);
+    
+    Material mat;
+    mat.DiffuseAlbedo = gDiffuseAlbedo;
+    mat.FresnelR0 = gFresnelR0;
+    mat.Shininess = 1.0f - gRoughness;
+    
+    float3 toEye = gEyePosW - pin.PosW;
+    
+    float3 ambient = gDiffuseAlbedo * gAmbientLight;
+    return float4(ambient + ComputeLighting(gLights, mat, pin.PosW, pin.NormalW, toEye), gDiffuseAlbedo.a);
+}
+
